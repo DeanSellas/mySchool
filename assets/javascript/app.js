ac936ce9b0d5f999e0e8aa6b6eae99fd9d0f1ac2
Dean <dgsellas@gmail.com>
2016-05-20 20:33:40 -0500

    basic calendar API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,22 @@
+/* JAVASCRIPT FOR DAY OF WEEK APP */
+
 var day;
 
 function setDay() {
-  day = "Gold Day"
-  $('p').text("Today is a "+day);
+
+  day = "Blue"
+  $('#day').text(day);
+
+  if(day === "Gold"){
+    $('#day').css({
+      "color":"yellow"
+    });
+  }
+  if(day === "Blue"){
+    $('#day').css({
+      "color":"blue"
+    });
+  }
 }
 
 window.onload = setDay;

9a8f837628c372c79ece10872f6fcb840d534043
Dean S <dgsellas@gmail.com>
2016-09-03 20:24:59 -0500

    working clubs angular

--- assets/javascript/controllers/angularControllers.js
+++ assets/javascript/controllers/angularControllers.js
@@ -1,14 +1,62 @@
+// CLUBS CONTROLLER
 angular.module('mySchoolApp')
     .controller("clubsController", function ($scope) {
+
+        // this is required to have variables work in the funtion
         var self = this;
         clubName;
         clubDescription;
-        clubAttendees;
+        clubMeeting = new Date();
 
-        self.logAllThings = function () {
+        self.addClub = function () {
 
             console.log("Club: " + self.clubName);
             console.log("Desciption: " + self.clubDescription);
-        };
 
+            var clubDate = $('#clubMeeting').val()
+            var dueDate = new Date(clubDate);
+
+            // +1 TO DATE, WHY? JAVASCRIPT IS WEIRD
+            dueDate.setDate(dueDate.getDate() + 1);
+
+            // EVENT TIME 8AM TO 3PM
+            var startTime = new Date(dueDate.setHours(8));
+            var endTime = new Date(dueDate.setHours(15));
+
+            // EVENT INFO
+            var clubEvent = {
+
+                "summary": "CLUB - " + self.clubName,
+                'description': 'club ' + self.clubName,
+
+                "start": {
+                    "dateTime": startTime
+                },
+
+                "end": {
+                    "dateTime": endTime
+                },
+            };
+
+            console.log(clubEvent);
+
+            // ADDS EVENT
+
+            var request = gapi.client.calendar.events.insert({
+                'calendarId': 'primary',
+                'resource': clubEvent
+            });
+
+            //IF EVENT CREATED
+            request.execute(function (clubEvent) {
+                //console.log(homeworkName + '\n' + className + '\n' + dueDate + '\n' + homeworkDescription);
+                console.log('Event created: ' + clubEvent.htmlLink);
+                alert("Club Added To Calendar");
+            });
+
+            // RESETS TEXT BOX
+            self.clubName = "";
+            self.clubDescription = "";
+
+        };
     });
